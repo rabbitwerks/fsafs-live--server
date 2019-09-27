b521aa2811b99ebc0f13e1cd5521bde752418b0e
SeanBecker <rabbitwerks.js@gmail.com>
2019-09-26 21:59:14 -0500

    update: register route for validation and password hashing, storing user in DB

--- src/app.js
+++ src/app.js
@@ -1,10 +1,14 @@
 const express = require('express');
+const volleyball = require('volleyball');
 
 const auth = require('./auth');
 
 const port = 1337;
 
 const app = express();
+
+app.use(volleyball);
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Welcome to the Fullstack Auth from Scratch backend!');
--- src/auth/index.js
+++ src/auth/index.js
@@ -1,12 +1,60 @@
 const router = require('express').Router();
-
+const Joi = require('@hapi/joi');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 const db = require('../db/connection');
 const users = db.get('users');
 
+const registerSchema = Joi.object().keys({
+  firstName: Joi.string().alphanum().min(3).max(30).required(),
+  lastName: Joi.string().alphanum().min(3).max(30).required(),
+  username: Joi.string().alphanum().min(3).max(30).required(),
+  userClass: Joi.string().alphanum().min(3).max(30).required(),
+  password: Joi.string().regex(/^[a-zA-Z0-9]{3,30}$/),
+  email: Joi.string().email({ minDomainSegments: 2 })
+});
+
+
 router.get('/register', (req, res) => {
   res.send('Welcome to the Register route!')
 });
+
+router.post('/register', (req, res, next) => {
+  const result = registerSchema.validate(req.body);
+  if(!result.error) {
+    users.findOne({
+      username: req.body.username,
+    }).then(user => {
+      if(user !== null) {
+        console.log('Username already exists, please choose another...');
+        const error = new Error('Username already exists, please choose another...');
+        next(error);
+      } else {
+        bcrypt.hash(req.body.password.trim(), saltRounds)
+          .then(hashedPassword => {
+            const newUser = {
+              firstName: req.body.firstName,
+              lastName: req.body.lastName,
+              username: req.body.username,
+              userClass: req.body.userClass,
+              email: req.body.email,
+              password: hashedPassword
+            };
+            users.insert(newUser)
+              .then(addedUser => {
+                delete addedUser.password;
+                res.json(newUser)
+              })
+              .catch(error => next(error))
+          })
+          .catch(error => next(error));
+      }
+    })
+  } else {
+    res.json(result.error);
+  }
+})
 
 router.get('/login', (req, res) => {
   res.send('Welcome to the Login route!')
