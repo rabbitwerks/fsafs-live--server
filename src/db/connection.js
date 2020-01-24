6ae9d0ca0660251f3c6f0844183884d044d05cb7
SEAN_RABBITWERKS <rabbitwerks.js@gmail.com>
2020-01-23 21:01:04 -0600

    updated db address to remote

--- src/db/connection.js
+++ src/db/connection.js
@@ -1,8 +1,8 @@
 const monk = require('monk');
 
 const local = 'localhost:27017/fsafs-live';
-const remote = '';
+const remote = 'mongodb+srv://rabbitwerks:<gax4-ic!-y4J2Hp>@userdb-ghjre.gcp.mongodb.net/test?retryWrites=true&w=majority';
 
-const db = monk(local, { useUnifiedTopology: true });
+const db = monk(remote, { useUnifiedTopology: true });
 
 module.exports = db;
