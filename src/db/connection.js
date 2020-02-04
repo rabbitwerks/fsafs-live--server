d2a55e4f7e4872a93785194d4b1773ab10a92e2d
SEAN_RABBITWERKS <rabbitwerks.js@gmail.com>
2020-02-03 22:32:02 -0600

    removed sens creds

--- src/db/connection.js
+++ src/db/connection.js
@@ -1,8 +1,7 @@
 const monk = require('monk');
 
 const local = 'localhost:27017/fsafs-live';
-const remote = 'mongodb+srv://rabbitwerks:<gax4-ic!-y4J2Hp>@userdb-ghjre.gcp.mongodb.net/test?retryWrites=true&w=majority';
 
-const db = monk(remote, { useUnifiedTopology: true });
+const db = monk(local, { useUnifiedTopology: true });
 
 module.exports = db;
